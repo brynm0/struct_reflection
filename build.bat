53320022c95e003e319f673675a478dcc4f34467
Bryn Murrell <brynmurrell97@gmail.com>
2020-02-21 17:30:47 +1100

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 set BLibPath= "W:\BLib"
 set JSONIncludePath="W:\json\include"
 
-set commonCompilerFlags= -W1 -arch:AVX2 -wd4556 -wd4805 -nologo -Gm- -GR- -EHsc- -Od -Oi -DPRINTINPUT=0 -DWIN32=1 -DDEBUG=1 -DPOOL_WIN32=1 -FC -Z7 -I %BLibPath% -I %JSONIncludePath%
+set commonCompilerFlags= -W1 -arch:AVX2 -wd4556 -wd4805 -nologo -Gm- -GR- -EHsc- -O2 -Oi -DPRINTINPUT=0 -DWIN32=1 -DDEBUG=1 -DPOOL_WIN32=1 -FC -Z7 -I %BLibPath% -I %JSONIncludePath%
 set commonLinkerFlags= -PROFILE -NODEFAULTLIB:msvcrt.lib -incremental:no -opt:ref user32.lib shell32.lib gdi32.lib winmm.lib 
 set renderExports=  -EXPORT:InitVulkan -EXPORT:BeginRender -EXPORT:DrawScreenSpaceGlyphEntities -EXPORT:DrawQuads -EXPORT:DrawScreenSpaceQuads -EXPORT:EndRender -EXPORT:DrawLineEntities -EXPORT:DrawPointEntities -EXPORT:CleanupVulkan -EXPORT:DrawStaticMeshEntities -EXPORT:EndRenderPass -EXPORT:BeginSecondaryRenderPass -EXPORT:DrawSDF -EXPORT:DrawTexturedQuadEntities -EXPORT:PrepareForRender -EXPORT:CopyMeshEntitiesToVertexBuffers
 REM 32 bit build
